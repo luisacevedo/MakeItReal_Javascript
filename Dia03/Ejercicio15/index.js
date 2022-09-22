b22f0a4dfe0f5c9173c0e8aeeea1bb554615d20b
acevedoluise <lea0133@hotmail.com>
2022-09-22 08:51:02 -0500

    Added functionality to Ejercicio15-console output. 	modified:   Dia03/Ejercicio15/index.js

--- Dia03/Ejercicio15/index.js
+++ Dia03/Ejercicio15/index.js
@@ -6,16 +6,28 @@ while(ver){
     if(!isNaN(num)){
         ver = false;
         let aux = 0;
+        let str = "(";
         for(let i = 1; i <= num; i++){
             aux += i;
+            if(!(i == num)){
+                str = str + i + "+";
+            }else{
+                str = str + i + ")";
+            }
         }
-        console.log(aux);
+        console.log(aux + str);
         aux = 0;
+        str = "(";
         while(index <= num){
-            aux += i;
+            aux += index;
+            if(!(index == num)){
+                str = str + index + "+";
+            }else{
+                str = str + index + ")";
+            }
             index++;
         }
-        console.log(aux);
+        console.log(aux + str);
     }else{
         console.log("¡Solo se admiten números!");
     }
